848b21fcfed83a3b01d9cbb8db7e23aaea8304d5
EzequielOmar <ezequiel.omar.vazquez@gmail.com>
2021-01-12 17:36:14 -0300

    setting webpack to compile all js in dist/bundle.js

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var path = require( 'path' );
+module.exports = {
+    mode: 'production',
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+};
